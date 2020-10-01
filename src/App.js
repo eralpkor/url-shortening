181fbc56cee765f0b3ec43f873df17f544727eea
eralpkor <eralp.ny@gmail.com>
2020-10-01 16:52:18 -0400

    Add component to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import Bottom from './components/Bottom';
 
 import { Route, Switch } from 'react-router-dom';
 
@@ -16,6 +17,7 @@ function App() {
           <Route exact path='/' render={() => <Main />} />
         </Switch>
       </div>
+      <Bottom />
       <Footer />
     </div>
     
